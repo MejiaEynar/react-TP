165bd10a191220a22ebffb1b3680df421c8605bb
MejiaEynar <eynarmejia03@gmail.com>
2023-09-28 10:08:56 -0300

    Css de New casi acabado, clear the objects useless

--- src/New.jsx
+++ src/New.jsx
@@ -5,17 +5,15 @@ function New(){
   
     return(
     <>
-    <div id='Formulario'>
         <h1>Nuevo Post</h1>
         <form>
             <input type="text" placeholder='Autor'/>
             <input type="text" placeholder='Titulo'/>
-            <textarea/>
+            <textarea placeholder='Escribir... '/>
             <div id='Boton'>
                 <input type="submit" value="Postear" />
             </div>
         </form>
-    </div>   
     </>
  )
 }
