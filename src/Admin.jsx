4ef0af3d66297c60c93de2911c3069e672738655
MejiaEynar <eynarmejia03@gmail.com>
2023-10-12 08:46:50 -0300

    as

---
+++ src/Admin.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+function Admin (props) {
+const {setAdmin} = props;
+const Navigate = useNavigate()
+const [contraseña, setContraseña] = useState ('')
+const handleSubmit = e => {
+    e.preventDefault()
+    if(contraseña == 'kakas'){
+        setAdmin(true)
+        Navigate('/')
+    }
+}
+
+return (
+    <>
+    <h1>¿Te atreves a ser ADMIN?</h1>
+<form onSubmit={handleSubmit}>
+<input onChange={e => setContraseña(e.target.value)}type="password" value={contraseña}/>
+<button type="submit">Entrar</button>
+</form>
+    </>
+)
+}
+export default Admin
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './styles/App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import New from './New';
+import Admin from './Admin';
 import Comentar from './Comentar';
 import { useState, useEffect } from 'react';
 
-function Inicio() {
+function Inicio(props) {
   const [publicaciones, setPublicaciones] = useState([]);
 
   useEffect(() => {
@@ -17,6 +18,13 @@ function Inicio() {
     localStorage.setItem('publicaciones', JSON.stringify([...publicaciones, nuevoPost]));
   };
 
+const borrarPublicaciones = (id) => {
+setPublicaciones(publicaciones.filter((contenido, index) => index != id))
+}
+const handleClick = e => {
+  borrarPublicaciones(e.target.id)
+}
+const {admin} = props;
   return (
     <>
       <div id='SearchBar'>
@@ -27,7 +35,9 @@ function Inicio() {
           </ul>
         </nav>
       </div>
+      <hr />
       <div>
+      {admin && <h3>Sos ADMIN, hace lo que se te canta el orto REY</h3>}
         {publicaciones && publicaciones.length > 0 ? (
           publicaciones.map((publicacion) => (
             <div key={publicacion.id} className='publicacion'>
@@ -35,6 +45,7 @@ function Inicio() {
               <h3>
                 <Link to={`/post/${publicacion.id}`}>
                   {publicacion.titulo}
+                  {admin && <button id={publicacion.id} onClick={handleClick}></button>}
                 </Link>
               </h3>
               <p>{publicacion.contenido}</p>
@@ -43,17 +54,20 @@ function Inicio() {
         ) : (
           <h4>No hay Publicaciones</h4>
         )}
+        {admin && <h3>Sos ADMIN, hace lo que se te canta el orto REY</h3>}
       </div>
     </>
   );
 }
 
 function App() {
+  const [admin, setAdmin] = useState(false)
   return (
     <Routes>
-      <Route path='/' element={<Inicio />} />
+      <Route path='/' element={<Inicio admin={admin}/>} />
       <Route path='/post/:id' element={<Comentar />} />
       <Route path='/new' element={<New />} />
+      <Route path='/admin' element={<Admin setAdmin={setAdmin}/>} />
     </Routes>
   );
 }
